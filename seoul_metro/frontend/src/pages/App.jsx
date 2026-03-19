7bd27a81fa04419380e2ecc724e4cb0e3cfbb410
unknown <nara1743@gmail.com>
2026-03-19 10:16:15 +0900

    ddl 수정

--- seoul_metro/frontend/src/pages/App.jsx
+++ seoul_metro/frontend/src/pages/App.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter, Routes, Route } from "react-router";
 
+
 const Home = () => {
   return (
     <div className="text-center">
